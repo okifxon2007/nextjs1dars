41521e372da86ae476bf8b45f015f2b2b10186bc
okifxon <shokirjonovokifxon46@gmail.com>
2024-10-03 10:36:22 +0300

    dars

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,86 @@
-import Image from "next/image";
+'use client'; 
 
-export default function Home() {
+import React, { useRef } from 'react';
+import '../app/styles/index.css';
+
+const Home = () => {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="hed-main">
+      <div className='conta'>
+        <header>
+          <h1>MNTN</h1>
+          <ul>
+            <li>Equipment</li>
+            <li>About us</li>
+            <li>Blog</li>
+          </ul>
+          <p>Account</p>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <main>
+          <div className="mainfollow">
+            <p>Follow us</p>
+          </div>
+          <div className="mainn">
+            <p>-----A Hiking guide</p>
+            <h1>Be prepared for the <br /> Mountains and beyond!</h1>
+            <h2>scroll down</h2>
+          </div>
+
+          <div className="pagess">
+            <ul>
+              <li onClick={() => scrollToSection(section1Ref)}>Start</li> <br />
+              <li onClick={() => scrollToSection(section2Ref)}>01</li> <br />
+              <li onClick={() => scrollToSection(section3Ref)}>02</li> <br />
+              <li>03</li>
+            </ul>
+            <hr />
+          </div>
+        </main>
+
+        {/* Bu yerda ref'larni qo'llash */}
+        <div className="maintwo" ref={section1Ref}>
+          <div className="mainpagess">
+            <div className="paragraph">
+              <h2>-----GEt Started</h2>
+              <h1>What level of hiker <br /> are you?</h1>
+              <p>Determining what level of hiker you are can be an important tool when planning future hikes...</p>
+              <span> read more- </span>
+            </div>
+            <img src="https://i.ytimg.com/vi/BirrM-u9xHs/maxresdefault.jpg" alt="" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="mainpagess" ref={section2Ref}>
+          <img src="https://i.pinimg.com/originals/a5/1a/cb/a51acbb0407a5a2f4400df65d970897b.jpg" alt="" />
+          <div className="paragraph">
+            <h2>-----Hiking Essentials</h2>
+            <h1>Picking the right <br /> Hiking Gear!</h1>
+            <p>The nice thing about beginning hiking is that you don’t really need any special gear...</p>
+            <span> read more- </span>
+          </div>
+        </div>
+
+        <div className="mainpagess" ref={section3Ref}>
+          <div className="paragraph">
+            <h2>-----where you go is the key</h2>
+            <h1>Understand Your  <br /> Map & Timing</h1>
+            <p>To start, print out the hiking guide and map...</p>
+            <span> read more- </span>
+          </div>
+          <img src="https://thumbs.dreamstime.com/b/male-hiker-looking-direction-magnetic-compass-mountains-fall-point-view-shot-man-s-hand-watch-bracelet-134178552.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Home;
